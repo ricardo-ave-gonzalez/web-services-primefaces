7a9e905fecf0af0bdc950edaa43b26ae49f038ec
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-05-28 13:53:20 -0300

    script run tomcat y jetty

--- proyecto-gestion/r.cmd
+++ proyecto-gestion/r.cmd
@@ -1,4 +1,4 @@
-mv target\ROOT \opt\apache-tomcat-9.0.62\webapps\
-cd \opt\apache-tomcat-9.0.62\bin\
+mv target\ROOT \opt\apache-tomcat-9.0.63\webapps\
+cd \opt\apache-tomcat-9.0.63\bin\
 java -cp tomcat-juli.jar;bootstrap.jar org.apache.catalina.startup.Bootstrap
-cd C:\cursos\galactica\clase5\web
+cd C:\ALABURAR\proyecto-gestion
